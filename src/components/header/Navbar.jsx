cc3418beb7dbd157de2e6e388b7ae8d2f3e05e1b
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-09-10 14:31:04 +0700

    feat: home page

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../../pages/Home";
+import Login from "../../pages/Login";
+import NotFound from "../../pages/NotFound";
+
+const NavBar = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">404: Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
